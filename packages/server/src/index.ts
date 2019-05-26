6f4cb2ee1e5c2a26d98105b7ce739f14e067f349
Lonny Gomes <code@lonnygomes.com>
2019-05-26 13:50:38 -0400

    Enable SSL for koa server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,5 @@
 import * as https from 'https';
+import * as fs from 'fs';
 import Koa from 'koa';
 
 const app = new Koa();
@@ -10,7 +11,15 @@ app.use(async (ctx: any) => {
     ctx.body = 'Hello World';
 });
 
-const httpServer = https.createServer(app.callback()).listen(HTTPS_PORT, () => {
-    const protocol = httpServer.addContext ? 'https' : 'http';
-    console.log(`Listening on ${protocol}://localhost:${HTTPS_PORT} ...`);
-});
+const sslOptions = {
+    key: fs.readFileSync('certs/server/server.local.key'),
+    cert: fs.readFileSync('certs/server/server.local.crt'),
+    ca: [fs.readFileSync('certs/ca/myCA.pem')],
+};
+
+const httpsServer = https
+    .createServer(sslOptions, app.callback())
+    .listen(HTTPS_PORT, () => {
+        const protocol = httpsServer.addContext ? 'https' : 'http';
+        console.log(`Listening on ${protocol}://localhost:${HTTPS_PORT} ...`);
+    });
