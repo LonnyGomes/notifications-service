01da1219b66c6e6bd4808ff73ccf44c49fc09cd7
Lonny Gomes <code@lonnygomes.com>
2019-05-25 20:46:28 -0400

    Add https server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,16 @@
-const Koa = require('koa');
+import * as https from 'https';
+import Koa from 'koa';
+
 const app = new Koa();
 
-app.use(async (ctx:any) => {
-  ctx.body = 'Hello World';
+// constants
+const HTTPS_PORT = 3001;
+
+app.use(async (ctx: any) => {
+    ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+const httpServer = https.createServer(app.callback()).listen(HTTPS_PORT, () => {
+    const protocol = httpServer.addContext ? 'https' : 'http';
+    console.log(`Listening on ${protocol}://localhost:${HTTPS_PORT} ...`);
+});
