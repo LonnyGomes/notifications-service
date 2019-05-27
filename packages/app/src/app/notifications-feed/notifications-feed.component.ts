cb0ba80838e0717075067a1050cf89e71b578e77
Lonny Gomes <code@lonnygomes.com>
2019-05-27 19:46:16 -0400

    Change notification array to receive the socket message model, #3
    Use unshift to pop the notification at the top of notifications array.

--- packages/app/src/app/notifications-feed/notifications-feed.component.ts
+++ packages/app/src/app/notifications-feed/notifications-feed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  SocketIoService,
+  SocketMessageModel
+} from '../services/socket-io.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notifications-feed',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-feed.component.scss']
 })
 export class NotificationsFeedComponent implements OnInit {
+  notifications: SocketMessageModel[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private socket: SocketIoService) {
+    this.notifications = [];
   }
 
+  ngOnInit() {
+    this.socket.observable.subscribe(model => {
+      if (!model) {
+        // ignore models that have no data
+        return;
+      }
+      this.notifications.unshift(model);
+    });
+  }
 }
