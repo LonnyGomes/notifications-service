fca4b00a67d901b3dd2b314f698f1bddfed2b0c0
Lonny Gomes <code@lonnygomes.com>
2019-05-26 20:35:28 -0400

    Test socket io connection in app

--- packages/app/src/app/services/socket-io.service.ts
+++ packages/app/src/app/services/socket-io.service.ts
@@ -8,8 +8,12 @@ export class SocketIoService {
   private socket = null;
   constructor() {
     this.socket = io('https://server.local:3001');
+    this.socket.on('news', data => {
+      console.log('got news', data);
+    });
     this.socket.on('connect', () => {
       console.log('we got it!');
     });
+    this.socket.emit('message', { my: 'data' });
   }
 }
