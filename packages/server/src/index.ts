973ee6f143abc7c13bd92a2f6d874377ea23a3c3
Lonny Gomes <code@lonnygomes.com>
2019-05-26 23:06:48 -0400

    Add error handling for server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -30,6 +30,27 @@ app.use(
     })
 );
 
+app.use(async function(ctx, next) {
+    try {
+        await next();
+    } catch (err) {
+        // some errors will have .status
+        // however this is not a guarantee
+        ctx.status = err.status || 500;
+        ctx.type = 'json';
+        ctx.body = {
+            status: 'failed',
+            message: err.message || 'Something Failed!',
+        };
+
+        // since we handled this manually we'll
+        // want to delegate to the regular app
+        // level error handling as well so that
+        // centralized still functions correctly.
+        ctx.app.emit('error', err, ctx);
+    }
+});
+
 router.get('/', async (ctx: any, next: any) => {
     const start = Date.now();
     await next();
