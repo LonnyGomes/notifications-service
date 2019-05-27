afc3b9a6574ab215f8725ad187688a4fcdf8575a
Lonny Gomes <code@lonnygomes.com>
2019-05-27 18:22:44 -0400

    Add material toolbar to application, #3

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationsFeedComponent } from './notifications-feed/notifications-feed.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotificationsFeedComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NotificationsFeedComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
