7fc251b2f7f8ba72578af097e22d5928ebeafcaf
Lonny Gomes <code@lonnygomes.com>
2019-05-27 20:08:31 -0400

    Import angular material cards, #3

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationsFeedComponent } from './notifications-feed/notifications-feed.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, NotificationsFeedComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
