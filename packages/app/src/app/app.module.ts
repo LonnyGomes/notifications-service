4333215187d00baca37f1e1326d82c0b98375b89
Lonny Gomes <code@lonnygomes.com>
2019-05-29 21:51:24 -0400

    Add material slider toggle for subscriptions, #9

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -4,21 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationsFeedComponent } from './notifications-feed/notifications-feed.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
-import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  declarations: [AppComponent, NotificationsFeedComponent, SubscriptionsComponent],
+  declarations: [
+    AppComponent,
+    NotificationsFeedComponent,
+    SubscriptionsComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
